898c6d47f96ef3e78f162534f220f9a9cb2ad95c
evertonulisystem <evertonulisystem@gmail.com>
2026-04-18 16:39:47 -0300

    feat aula 42: iniciando com testes no playwright

---
+++ tests/categories/categories.list.spec.ts
@@ -0,0 +1,8 @@
+import { test, request, expect } from "@playwright/test";
+
+test('não permite listar categorias sem autenticação', async({ request }) => {
+    const response = await request.get('https://northwind-test-platform.vercel.app/api/v1/categories')
+    //console.log(response.status());
+    expect(response.status()).toBe(401);
+});
+
