26c79b990fb2e6dcfada67044ed68ea594446e85
evertonulisystem <evertonulisystem@gmail.com>
2026-04-19 15:46:31 -0300

    feat (aula44): eliminação de repetição e beforeEach

--- tests/categories/categories.list.spec.ts
+++ tests/categories/categories.list.spec.ts
@@ -1,25 +1,37 @@
-import { test, request, expect } from "@playwright/test";
+import { test, request, expect, APIResponse } from "@playwright/test";
 
 
 test.describe('Listagem Categorias', () => {
 
     test.describe('Listagem completa', () => {
 
+
+        let response: APIResponse;
+        test.beforeEach(async ({ request }) => {
+            response = await request.get('categories');
+        });
+
+        test('debug baseURL', async () => {
+
+            console.log('URL FINAL:', response.url());
+        });
+
         test('não permite listar categorias sem autenticação', async ({ request }) => {
-            const response = await request.get('https://northwind-test-platform.vercel.app/api/v1/categories')
+
             //console.log(response.status());
+            console.log('URL FINAL:', response.url());
             expect(response.status()).toBe(401);
 
         });
 
         test('explorando responses - verificando status code', async ({ request }) => {
-            const response = await request.get('https://northwind-test-platform.vercel.app/api/v1/categories')
+
             console.log('Status Code:', response.status());
 
         });
 
         test('explorando responses', async ({ request }) => {
-            const response = await request.get('https://northwind-test-platform.vercel.app/api/v1/categories')
+
             console.log('Status Code:', response.status());
             console.log('URL do Sistema:', response.url());
 
@@ -31,7 +43,7 @@ test.describe('Listagem Categorias', () => {
 
 
         test('impedir validação de  categoria por id sem autenticação', async ({ request }) => {
-            const response = await request.get('https://northwind-test-platform.vercel.app/api/v1/categories/3')
+            const response = await request.get('categories/3')
             expect(response.status()).toBe(401);
             console.log('Status Code:', response.status());
 
