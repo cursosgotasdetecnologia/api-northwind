bdb82ecd72e3f6b0a2cd19f345b05fb1bf364116
evertonulisystem <evertonulisystem@gmail.com>
2026-04-21 10:58:52 -0300

    feat (aula46): uso de  bearer token

--- tests/products/products.list.spec.ts
+++ tests/products/products.list.spec.ts
@@ -1,16 +1,31 @@
 import { test, request, expect, APIResponse } from "@playwright/test";
 
 test.describe('Listagem de Produtos', () => {
-    
-    test('', () => {
-        
+
+    let token: string;
+
+    test.beforeEach(async ({ request }) => {
+
+        const login = await request.post('auth/login', {
+            data: {
+                email: "admin@qatest.com",
+                password: "Teste@123"
+            }
+        })
+        const body = await login.json();
+        token = body.data.token;
     });
 
 
+    test('Acesso com sucesso ao endpoint de filtro de produtos', async ({ request }) => {
 
+        const response = await request.get('products?page=1&limit=10', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        expect(response.status()).toBe(200);
+        console.log('O token é: ', token)
 
-
-
-
-
+    });
 });
